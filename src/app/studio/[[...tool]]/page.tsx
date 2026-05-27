4fcb5651206c86956bbe994eb892a513a9772fc0
Jonathan <jontezlitz@gmail.com>
2026-05-27 17:19:02 +0200

    chore: remove dead fonts, dead globs, dead vars; mark studio dynamic

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -3,7 +3,9 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
-export const dynamic = 'force-static'
+// Studio är en interaktiv klient-applikation som bootstrapper i webbläsaren.
+// `force-dynamic` säkerställer att Next inte försöker prerendra något ISR-state.
+export const dynamic = 'force-dynamic'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
